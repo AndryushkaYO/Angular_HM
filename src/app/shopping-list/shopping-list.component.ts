920a4ee9555ca37e20e36e2e68a5c9c9abc0f2ce
Andrianna Yovbak <Andrianna_Yovbak@epam.com>
2019-07-05 15:44:56 +0300

    Added recipe detail block & possibility to add an ingradient to shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,8 @@ export class ShoppingListComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  addNewIngradient(product:Ingradient) {
+    this.ingradients.push(product);
+  }
 }
